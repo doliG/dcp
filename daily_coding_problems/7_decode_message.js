85ca3f79cc256b13a0ec1dc2b6d8a7e7a41937ae
doliG <guillaume.lodi@gmail.com>
2020-04-01 11:11:55 +0200

    DCP#7 Add memoization

--- daily_coding_problems/7_decode_message.js
+++ daily_coding_problems/7_decode_message.js
@@ -6,15 +6,22 @@ For example, the message '111' would give 3, since it could be decoded as 'aaa',
 You can assume that the messages are decodable. For example, '001' is not allowed.
 */
 
-function resolve(message) {
+function resolve(message, memo = {}) {
   if (message.length <= 1) {
     return 1;
   }
-  let res = resolve(message.slice(1));
-  if (parseInt(message.slice(0, 2)) <= 26) {
-    res += resolve(message.slice(2));
+  if (memo[message.length]) {
+    return memo[message.length];
   }
+  let res = resolve(message.slice(1), memo);
+  if (parseInt(message.slice(0, 2)) <= 26) {
+    res += resolve(message.slice(2), memo);
+  }
+  memo[message.length] = res;
   return res;
 }
+
+// Thanks you @Georgios Darkos for you explainations on memoization
+// https://medium.com/@george.drakos62/interview-coding-problems-1-b0a7f911466
 
 module.exports = resolve;
