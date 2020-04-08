75005dbd1b27429a9d9940758e158a950a0de259
Guillaume Lodi <guillaume.lodi@gmail.com>
2020-04-08 14:54:51 +0200

    Cleaning

--- daily_coding_problems/src/1_sum_completion.js
+++ daily_coding_problems/src/1_sum_completion.js
@@ -1,19 +1,3 @@
-const assert = require("assert").strict;
-
-// Args formats = { args: [list, k], expected: expected function return }
-const tests = [
-  { args: [[10, 15, 3, 7], 17], expected: true },
-  { args: [[10, 15, 3, 7], 18], expected: true },
-  { args: [[10, 15, 3, 7], 1], expected: false },
-  { args: [[], 10], expected: false }
-];
-
-// for (test of tests) {
-//   assert.deepEqual(resolveDumb(...test.args), test.expected);
-//   assert.deepEqual(resolveReadable(...test.args), test.expected);
-//   assert.deepEqual(resolveSmart(...test.args), test.expected);
-// }
-
 function resolveDumb(list, k) {
   for (let i = 0; i < list.length; i++) {
     for (let j = i + 1; j < list.length; j++) {
@@ -27,7 +11,7 @@ function resolveDumb(list, k) {
 
 function resolveReadable(list, k) {
   for (const elem of list) {
-    if (list.find(n => n + elem === k)) {
+    if (list.find((n) => n + elem === k)) {
       return true;
     }
   }
