01b3f920fc269d37b477dcc260ea02d87e43e3e6
Guillaume Lodi <guillaume.lodi@vestiairecollective.com>
2020-05-12 10:59:17 +0200

    DCP#11 Add trie insert implementation

--- daily_coding_problems/src/11_autocomplete.js
+++ daily_coding_problems/src/11_autocomplete.js
@@ -12,13 +12,50 @@ speed up queries.
 Resources:
 - https://www.wikiwand.com/fr/Trie_(informatique)
 - https://medium.com/@daetam/trie-autocomplete-8dd23ddd3846
+- https://www.youtube.com/watch?v=-urNrIAQnNo
+- https://www.wikiwand.com/en/Trie
 */
 
 // Brut force, fair enough for frontend, can be better with tries
-function autoComplete(query, list) {
-  return list.filter(s => s.startsWith(query))
+// function autoComplete(query, list) {
+//   return list.filter(s => s.startsWith(query))
+// }
+
+// With trie, thanks wikipedia
+class Node {
+  constructor(value) {
+    this.children = {}
+    this.value = value
+  }
 }
 
-// console.log(autoComplete('de', ['dog', 'deer', 'deal']));
+class Trie {
+  constructor() {
+    this.root = new Node()
+  }
+
+  insert(key) {
+    let node = this.root;
+
+    for (const char of key) {
+      if (!(char in node.children)) {
+        node.children[char] = new Node()
+      }
+      node = node.children[char]
+    }
+    node.value = key
+  }
+
+  // Return all elements contained by the Trie matching regexp as a list
+  findAll(regexp) {
+    return []
+  }
+}
+
+function autoComplete(query, list) {
+  const trie = new Trie()
+  list.forEach(word => trie.insert(word))
+  return trie.findAll(new RegExp(`^${query}`))
+}
 
 module.exports = autoComplete;
